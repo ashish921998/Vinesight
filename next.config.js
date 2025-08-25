9b9b3e60223678eb57ad5387ce16d50fe706e192
Ashish Huddar <ashishhuddar@Ashishs-MacBook-Pro.local>
2025-08-25 21:04:25 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -3,15 +3,12 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
   },
